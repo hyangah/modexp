f7a3b6ff6263c03f1b2926452c692a71ef0502f8
Hana Kim <hyangah@gmail.com>
2018-11-08 11:58:51 -0500

    Initial commit

---
+++ bin/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/hyangah/modexp/lib1"
+
+func main() {
+	lib1.Hello()
+}
---
+++ lib2/lib2.go
@@ -0,0 +1,8 @@
+package lib2
+
+import "github.com/hyangah/modexp/lib1"
+
+func Hello() {
+	lib1.Hello()
+	println("How are you?")
+}
